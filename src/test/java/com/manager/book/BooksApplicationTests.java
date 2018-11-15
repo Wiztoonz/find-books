8e8814805ea72980678566d61e8b8b21c76f9369
oleg <Wiztoonz4@gmail.com>
2018-11-15 19:14:24 +0200

    add invoke tests

--- src/test/java/com/manager/book/BooksApplicationTests.java
+++ src/test/java/com/manager/book/BooksApplicationTests.java
@@ -1,16 +1,67 @@
 package com.manager.book;
 
+import com.manager.book.model.Books;
+import com.manager.book.repository.BookJpaRepository;
+import com.manager.book.service.BookService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import java.util.List;
+
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
 public class BooksApplicationTests {
 
+    @Mock
+    BookJpaRepository bookJpaRepository;
+    @InjectMocks
+    BookService bookService;
+
     @Test
-    public void contextLoads() {
+    public void testUseFindAll() throws Exception {
+        // testing
+        List<Books> all = bookService.findAll();
+        // validate
+        verify(bookJpaRepository).findAll();
+    }
+
+    @Test
+    public void testUseFindById() throws Exception {
+        // testing
+        Books byName = bookService.findByName(1);
+        // validate
+        verify(bookJpaRepository).findById(1);
+    }
+
+    @Test
+    public void testUseSave() throws Exception {
+        Books books = new Books();
+        // testing
+        bookService.save(books);
+        // validate
+        verify(bookJpaRepository).save(books);
+    }
+
+    @Test
+    public void testUseUpdate() throws Exception {
+        Books books = new Books();
+        // testing
+        bookService.updateBook(1, books);
+        // validate
+        verify(bookJpaRepository).save(books);
+    }
+
+    @Test
+    public void testUseDelete() throws Exception {
+        Books books = new Books();
+        // testing
+        bookService.deleteBook(books);
+        // validate
+        verify(bookJpaRepository).delete(books);
     }
 
 }
